3c4e21081b3bdf95ebe71078bc35bcfffe13baae
aljaljul <aljaljul@LAPTOP-SN7U10PQ>
2023-11-12 04:03:49 -0500

    fixing continues

--- p2test/asgi.py
+++ p2test/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for csc309_p2 project.
+ASGI config for p2test project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csc309_p2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p2test.settings')
 
 application = get_asgi_application()
--- p2test/wsgi.py
+++ p2test/wsgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for csc309_p2 project.
+WSGI config for p2test project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csc309_p2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p2test.settings')
 
-application = get_asgi_application()
+application = get_wsgi_application()
