de0bffd6ba9b5eeb7e11ddaa446267674db4eb87
aljaljul <aljaljul@LAPTOP-SN7U10PQ>
2023-11-12 23:02:39 -0500

    adding permissions to pets

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PetListCreateView, PetRetrieveUpdateDestroyView
+from .views import PetListView, PetRetrieveUpdateDestroyView, PetCreateView
 
 urlpatterns = [
-    path('pets/', PetListCreateView.as_view(), name='pet-list-create'),
+    path('pets/', PetListView.as_view(), name='pet-list'),
+    path('pets/create/', PetCreateView.as_view(), name='pet-create'),
     path('pets/<int:pk>/', PetRetrieveUpdateDestroyView.as_view(), name='pet-retrieve-update-destroy'),
 ]
