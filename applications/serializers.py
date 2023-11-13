f47b948344de16fc53e0720a5670bb3742b53f41
Zein Sulayman <zein.sulayman@mail.utoronto.ca>
2023-11-12 21:23:34 -0500

    applications first commit. first attempt with all files added, in preparation for session with TA

---
+++ applications/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import PetApplication
+
+
+class PetApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PetApplication
+        fields = '__all__'
+
+
+class PetApplicationUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PetApplication
+        fields = ['status']
+
+    def validate_status(self, value):
+        # Validate the allowed status transitions based on user type
+        user_is_shelter = self.context['request'].user.is_shelter
+        current_status = self.instance.status if self.instance else None
+
+        if user_is_shelter:
+            if current_status == PetApplication.PENDING and value not in [PetApplication.ACCEPTED, PetApplication.DENIED]:
+                raise serializers.ValidationError('Invalid status transition for shelter')
+        else:
+            if current_status in [PetApplication.PENDING, PetApplication.ACCEPTED] and value != PetApplication.WITHDRAWN:
+                raise serializers.ValidationError('Invalid status transition for seeker')
+
+        return value
